4751dea57e6ad972b787eac7897dac0e4be738d4
ztanji <zachary.tanji@etu.u-bordeaux.fr>
2023-01-31 14:23:54 +0100

    FermTransAttr

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def main():
-    print("Hello World!")
 
-if __name__ == "__main__":
-    main()
+def FermTransAttr(F, A):
+    A_plus = set(A)
+    while True:
+        A_plus_tmp = set(A_plus)
+        for X, Y in F:
+            if X.issubset(A_plus):
+                A_plus |= Y
+        if A_plus == A_plus_tmp:
+            break
+    return A_plus
+
+F = [
+    (set('c'), set('a')),
+    (set({'c', 'e'}), set({'b','d'})),
+    (set({'a', 'b','d'}), set('b')),
+    (set('d'), set('e')),
+    (set({'b', 'c'}), set('d')),
+]
+
+assert FermTransAttr(F, set()) == set()
+assert FermTransAttr(F, {'b', 'e'}) == {'b', 'e'}
+assert FermTransAttr(F, {'c'}) == {'a', 'c'}
+assert FermTransAttr(F, {'a', 'd'}) == {'a', 'd', 'e'}
+assert FermTransAttr(F, {'c', 'd'}) == {'a', 'b', 'c', 'd', 'e'}
