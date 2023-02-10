150ec3cce7dcd640a62c35fc1d8a8f06d7a9c85e
ztanji <zachary.tanji@etu.u-bordeaux.fr>
2023-02-10 15:50:24 +0100

    Génération

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 import copy
+import random
 
 def convert_txt_to_list(file_name: str) -> List[List[str]]:
     list_of_lists = []
@@ -28,7 +29,6 @@ def FermTransAttr(F: List[List[str]], A: List[str]) -> List[str]:
 
 def CouvMinDF(F: List[List[str]]) -> List[List[str]]:
     C = []
-
     for X, Y in F:
         for y in Y:
             C.append([X, [y]])
@@ -36,10 +36,9 @@ def CouvMinDF(F: List[List[str]]) -> List[List[str]]:
     i = 0
     while i < len(C):
         current_line = C[i]
-        Ctmp = copy.deepcopy(C)
-        del Ctmp[i]
+        Ctmp = C[:i] + C[i+1:]
         if all(item in FermTransAttr(Ctmp, current_line[0]) for item in current_line[1]):
-            del C[i]
+            C.pop(i)
             i -= 1
         i += 1
 
@@ -49,12 +48,9 @@ def CouvMinDF(F: List[List[str]]) -> List[List[str]]:
         for key in lst_key:
             values_to_test = list(lst_key)
             values_to_test.remove(key)
-
-            Ctmp = copy.deepcopy(C)
-            del Ctmp[i]
-
+            Ctmp = C[:i] + C[i+1:]
             if key in FermTransAttr(Ctmp, values_to_test):
-                C[i][0] = values_to_test
+                C[i][0] = [x for x in C[i][0] if x != key]
                 i -= 1
                 break
         i += 1
@@ -132,38 +128,72 @@ def _display_decomposition(F: List[List[str]]) -> None:
             print(x, end='')
         print(")")
 
-
-
-def follow_letter_chain(file, letters):
-    # Create an empty output
-    output = ""
-     
-    # Read file line by line
-    with open(file) as f:
-        for line in f:
-        
-            # Get words from line
-            split_line = line.split("->")
-            
-            # Get source and target from line
-            source, target = split_line[0], split_line[1].strip()
-            
-            # Check if source of line is in letters
-            if source in letters:
-                
-                # Add associated target words to output
-                output += target + " "
-                
-                # Replace source with associated target words
-                letters = letters.replace(source, target)
+def letter_to_file(letter: str) -> None:
+    # Create file
+    file = open("jeu_donnée.txt", "w")
     
-    # Return output
-    return output
+    # Transform letter in a list
+    letter_list = list(letter.lower())
+
+    # Iterate through each letter in the list
+    for l in letter_list:
+        int1 = random.randint(1, len(letter_list))
+        int2 = random.randint(1, len(letter_list))
+        rand_letters = []
+        rand_right_letters = []
+            
+        # Get two random numbers and use them to choose two random letters
+        for i in range(int1):
+            rand_letter = random.choice(letter_list)
+            rand_letters.append(rand_letter)
+
+        for i in range(int2):
+            right_letter = random.choice(letter_list)
+            while right_letter in rand_letters:
+                right_letter = random.choice(letter_list)
+            rand_right_letters.append(right_letter)
+
+        # Check that the same letter is not written twice
+        for l in rand_right_letters:
+            if l in rand_right_letters:
+                rand_right_letters.remove(l)
+
+        for l in rand_letters:
+            if l in rand_letters:
+                rand_letters.remove(l)
+
+        # Check if the list are not empty
+        if len(rand_letters) == 0:
+            rand_letters.append(random.choice(letter_list))
+        if len(rand_right_letters) == 0:
+            rand_right_letters.append(random.choice(letter_list))
+            
+        # Write chosen letters and matching answer to file
+        for l in rand_letters:
+            file.write(l)
+            if l != rand_letters[-1]:
+                file.write(",")
+        file.write(" -> ")
+        for l in rand_right_letters:
+            file.write(l)
+            if l != rand_right_letters[-1]:
+                file.write(",")
+        file.write("\n")
+    file.close()
+    file = open("jeu_donnée.txt", "r")
+    lines = file.readlines()
+    for line in lines:
+        line.replace(",\n", "\n")
+    file.close()
 
 
-follow_letter_chain("test.txt", "Zachary")
 
+
+
+
+
+letter_to_file("Zachary")
 
 print(convert_txt_to_list('HTS+_simplif.txt'))
 print("====== CouvMinDF ======")
-_display_decomposition(DecompoDFen3FN(convert_txt_to_list('HTS+_simplif.txt'),['a','c','e']))
+_display(CouvMinDF(convert_txt_to_list('HTS+_simplif.txt')))
